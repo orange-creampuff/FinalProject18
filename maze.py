c46e2ae8c8ed88aea14455b5db877d959056dea1
OrangeCreampuff <mfleishman@ucvts.org>
2019-01-03 15:24:04 +0000

    Added new text to maze 3

--- maze.py
+++ maze.py
@@ -14,21 +14,34 @@ maze_number = int(input("Pick a number from 0 to 9"))
 #Two piece maze
 if maze_number == 3:
     location = 1
-    if location == 1:
-        print("""The arrangement of squares is 1 2 3
-                                4 5 6
-                                7 8 9.
-        You may move to any adjacent square, unless the path is blocked.
-        You are in square 1. The exit is in square 8.
-        A cobra blocks the path to square 4. You must proceed to square 2.""")
-        location == 2
-    if location == 2:
-        print("""1 2 3
-    4 5 6
-    7 8 9.
-You may proceed to square 5 or square 3. What do you pick?""")
-        int(input("5 or 3?"))
-    if location == 3:
-        print("""1 2 3
-    4 5 6
-    7 8 9.""")
+    def show_progress():
+            """Show how you are doing"""
+            print(f"""The arrangement of squares is 1 2 3
+                              4 5 6
+                              7 8 9.
+            You may move to any adjacent square, unless the path is blocked.
+            You are in square {location}. The exit is in square 8.""")
+    while True:
+
+        if location == 1:
+            show_progress()
+            print("A cobra blocks the path to square 4. You must proceed to square 2.")
+            location = 2
+        if location == 2:
+            show_progress()
+            print("""
+    You may proceed to square 1, square 3, or square 5. What do you pick?""")
+            x = input("1, 3 or 5?")
+            if x == "1":
+                location = 1
+            if x == "3":
+                location = 3
+            if x == "5":
+                location = 5
+        if location == 3:
+            show_progress()
+            print("""
+    A gate slams behind you. You may not go back to square 2. You must proceed to square 6""")
+            location = 6
+        if location == 6:
+            print()
